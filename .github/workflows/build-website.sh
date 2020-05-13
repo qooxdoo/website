54f3d7ada4fb14395823aa18072a3127cf367e8d
Christian Boulanger <info@bibliograph.org>
2020-05-13 23:18:00 +0200

    Update build-website.sh

--- .github/workflows/build-website.sh
+++ .github/workflows/build-website.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
 DEPLOY_PK="$1"
-
 RSYNC_OPTS="--recursive --perms --times --group --owner --devices --specials --verbose --copy-links --copy-dirlinks --delete"
-
 mkdir tmp 
 ABSOLUTE_TMP=$(echo "$(cd "$(dirname "$tmp")"; pwd -P)/$(basename "tmp")")
 echo "$DEPLOY_PK" > ./tmp/deploy-key
 chmod 0600 ./tmp/deploy-key
-
-git clone https://github.com/qooxdoo/qooxdoo-compiler.git --depth=1 --single-branch ./tmp/qooxdoo-compiler
-cp -R tmp/qooxdoo-compiler/source/resource/qx/tool/schema ./html
 npm i
+
+echo
+echo ">>> Building website..."
 npx gulp
 
-git clone https://github.com/qooxdoo/qooxdoo.git --depth=1 --single-branch ./tmp/qooxdoo
-cp -r ./tmp/qooxdoo/docs html
-
+echo ">>> Cloning existing website..."
 git config --global user.email "deployment@qooxdoo.org"
 git config --global user.name "Automated Deployment for qooxdoo/website"
-
 git clone -c core.sshCommand="/usr/bin/ssh -i $ABSOLUTE_TMP/deploy-key" git@github.com:qooxdoo/qooxdoo.github.io.git --depth=1 ./tmp/qooxdoo.github.io
 
+echo ">>> Adding json-schema files..."
+git clone https://github.com/qooxdoo/qooxdoo.git --depth=1 --single-branch ./tmp/qooxdoo
 rsync $RSYNC_OPTS ./tmp/qooxdoo/docs ./tmp/qooxdoo.github.io
 
+echo ">>> Adding docs..."
+git clone https://github.com/qooxdoo/qooxdoo-compiler.git --depth=1 --single-branch ./tmp/qooxdoo-compiler
+rsync $RSYNC_OPTS ./tmp/qooxdoo-compiler/source/resource/qx/tool/schema ./tmp/qooxdoo.github.io
+
+echo ">>> Synchronzing website content..."
 cd html
 rsync $RSYNC_OPTS \
     --exclude=docs \
